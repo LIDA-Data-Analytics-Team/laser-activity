1214d1683199d60269444c07b8d897b867401de2
Adam Keeley <uitake@leeds.ac.uk>
2023-03-20 12:05:42 +0000

    Called methods from __init__

--- laser_resources/__init__.py
+++ laser_resources/__init__.py
@@ -2,8 +2,16 @@ import datetime
 import logging
 
 import azure.functions as func
+from .LASER_resources import resourceGroups, resources
+import pandas as pd
 
+def main(mytimer: func.TimerRequest) -> None:
+    df_rg = resourceGroups()
+    print(f"Resource Group count: {df_rg.shape[0]}")
+    df_r = resources(df_rg['ResourceGroup'].to_list())
+    print(f"Resource count: {df_r.shape[0]}")
 
+'''
 def main(mytimer: func.TimerRequest) -> None:
     utc_timestamp = datetime.datetime.utcnow().replace(
         tzinfo=datetime.timezone.utc).isoformat()
@@ -12,3 +20,4 @@ def main(mytimer: func.TimerRequest) -> None:
         logging.info('The timer is past due!')
 
     logging.info('Python timer trigger function ran at %s', utc_timestamp)
+'''
